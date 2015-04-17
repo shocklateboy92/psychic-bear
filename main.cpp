337c6916ae1d506ad4b28d802ba09474b3762c0b
Lasath Fernando <devel@lasath.org>
2015-04-17 13:58:36 +1000

    Start evaluating Character mock
    The best way of finding how incomplete something is, is to launch it.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlComponent>
+
+#include <src/attribute-manager.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    qmlRegisterType<Attribute>("org.lasath.psychic_bear", 1, 0, "Attribute");
+    qmlRegisterType<Bonus>("org.lasath.psychic_bear", 1, 0, "Bonus");
+
+    QQmlComponent character(&engine);
+    character.loadUrl(QUrl(QStringLiteral("qrc:/Character.qml")));
+
+    for (Attribute* a : AttributeManager::instance().attributes()) {
+        qDebug() << a->objectName();
+    }
+
+//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/Character.qml")));
 
     return app.exec();
 }
--- psychic-bear.pro
+++ psychic-bear.pro
@@ -1,12 +1,17 @@
 TEMPLATE = app
 
 QT += qml quick widgets
+CONFIG += c++14
+
+QMAKE_CXXFLAGS += -Wall
 
 SOURCES += main.cpp \
     src/attribute.cpp \
-    src/bonus.cpp
+    src/bonus.cpp \
+    src/attribute-manager.cpp
 
-RESOURCES += qml.qrc
+RESOURCES += qml.qrc \
+    sheet/character.qrc
 
 # Additional import path used to resolve QML modules in Qt Creator's code model
 QML_IMPORT_PATH =
@@ -16,4 +21,5 @@ include(deployment.pri)
 
 HEADERS += \
     src/attribute.h \
-    src/bonus.h
+    src/bonus.h \
+    src/attribute-manager.h
