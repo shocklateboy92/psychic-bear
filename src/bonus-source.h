57eb4f9aa42fb3cc56b4d64f81dac089150f7165
Lasath Fernando <devel@lasath.org>
2015-04-30 00:24:55 +1000

    Add URI property to BonusSource
    At the moment, it doesn't do anything with the property, but it's there.
    Also, I wonder if it's worthwhile collecting all the objects with URIs
    into a base class (maybe called Resource).
    
    In any case, I'll duplicate it for now and change it if any more things
    end up needing a URI.

--- src/bonus-source.h
+++ src/bonus-source.h
@@ -9,21 +9,28 @@ class PB_SHARED_EXPORT BonusSource : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString uri READ uri WRITE setUri NOTIFY uriChanged)
 
 public:
+    using List = QList<BonusSource*>;
+
     explicit BonusSource(QObject *parent = 0);
     ~BonusSource();
 
     QString name() const;
+    QString uri() const;
 
 signals:
     void nameChanged(QString arg);
+    void uriChanged(QString arg);
 
 public slots:
     void setName(QString arg);
+    void setUri(QString arg);
 
 private:
     QString m_name;
+    QString m_uri;
 };
 
 #endif // BONUSSOURCE_H
