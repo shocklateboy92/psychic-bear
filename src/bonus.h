0729ef5d6df2eddcc11d481ecf3f7c77271b1177
Nathan Smyth <nathan@foodbanklocal.com>
2015-06-09 16:41:48 +1000

    Updating bonus to use resource

--- src/bonus.h
+++ src/bonus.h
@@ -3,17 +3,17 @@
 
 #include <QObject>
 #include "pb-core.h"
+#include "resource.h"
 
 class BonusSource;
 class Attribute;
 class BonusType;
 
-class PB_SHARED_EXPORT Bonus : public QObject
+class PB_SHARED_EXPORT Bonus : public Resource
 {
     using Source = BonusSource*;
 
     Q_OBJECT
-    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
     Q_PROPERTY(int amount READ amount WRITE setAmount NOTIFY amountChanged)
     Q_PROPERTY(BonusSource* source READ source WRITE setSource NOTIFY sourceChanged)
     Q_PROPERTY(Attribute* target READ target WRITE setTarget NOTIFY targetChanged)
@@ -33,21 +33,18 @@ public:
 
 signals:
     void amountChanged(int arg);
-    void nameChanged(QString arg);
     void sourceChanged(Source arg);
     void targetChanged(Attribute* arg);
     void typeChanged(BonusType* type);
 
 public slots:
     void setAmount(int arg);
-    void setName(QString arg);
     void setSource(Source arg);
     void setTarget(Attribute* arg);
     void setType(BonusType* type);
 
 private:
     int m_amount;
-    QString m_name;
     Attribute* m_target;
     Source m_source;
     BonusType* m_type;
