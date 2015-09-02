c13e363e4775a056eb24407a29bfe0ec11a28459
Lasath Fernando <devel@lasath.org>
2015-09-03 09:12:37 +1000

    Start implementing the menu bar

--- app/container-window.cpp
+++ app/container-window.cpp
@@ -4,6 +4,8 @@
 #include <QtQuickWidgets/QQuickWidget>
 
 #include <QDockWidget>
+#include <QMenu>
+#include <QMenuBar>
 
 const QStringList ContainerWindow::MODULE_SRC_PATHS = {
     "qrc:/ui/scores/AbilityScoresModule.qml",
@@ -23,6 +25,22 @@ void ContainerWindow::setupUi()
                    | QMainWindow::AllowTabbedDocks
                    | QMainWindow::AnimatedDocks);
 
+    QMenu *file = new QMenu("File");
+    menuBar()->addMenu(file);
+
+    QMenu *views = new QMenu("View");
+    menuBar()->addMenu(views);
+
+    QAction *quit = new QAction(this);
+    quit->setText("Exit");
+    quit->setShortcut(QKeySequence::Quit);
+    connect(quit, &QAction::triggered, this, &ContainerWindow::close);
+    file->addAction(quit);
+
+    QMenu *help = new QMenu("Help");
+    help->addAction("Lol No!");
+    menuBar()->addMenu(help);
+
     for (QString path : MODULE_SRC_PATHS) {
         QQuickWidget *widget = new QQuickWidget;
         widget->setSource(QUrl(path));
@@ -33,6 +51,18 @@ void ContainerWindow::setupUi()
 
         QDockWidget *dock = new QDockWidget(module->name());
         dock->setWidget(widget);
+
+        QAction *action = new QAction(this);
+        action->setText(module->name());
+        action->setCheckable(true);
+        views->addAction(action);
+
+        connect(action, &QAction::triggered, [=](){
+            dock->setVisible(action->isChecked());
+        });
+        connect(dock, &QDockWidget::visibilityChanged, [=](){
+            action->setChecked(dock->isVisible());
+        });
 
         addDockWidget(Qt::LeftDockWidgetArea, dock);
     }
