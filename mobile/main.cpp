631c9db9905d39bc48adeee4712f69d13107790a
Lasath Fernando <kde@lasath.org>
2015-11-25 00:26:17 +1100

    Make mobile init/use db
    This is ghetto copy-pasted code from app.
    TODO: Make a way of sharing code between app and mobile.

--- mobile/main.cpp
+++ mobile/main.cpp
@@ -1,5 +1,7 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QSqlDatabase>
+#include <QSqlError>
 #include <core_plugin.h>
 #include <project-context.h>
 #include "../app/ui-module.h"
@@ -14,6 +16,29 @@ int main(int argc, char *argv[])
     ProjectContext context;
     context.createCharacter(QUrl(QStringLiteral("qrc:/sheet/%1.qml").arg("fernie/Character")));
 
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    if (argc > 1) {
+        db.setDatabaseName(argv[1]);
+    } else {
+        db.setDatabaseName("/sdcard/Download/psychic_bear.db");
+    }
+    bool open = db.open();
+    if (!open) {
+        qWarning() << "Failed to open database :" << db.lastError();
+        qWarning() << "Runtime Static Modifiers are disabled";
+    }
+
+    QSqlQuery().exec("PRAGMA foreign_keys = ON;");
+
+    for (Resource *a : context.allResources()) {
+        if (a->isDynamic()) {
+            bool success = a->initDb();
+            if (!success) {
+                qWarning() << "failed to initialize DB for" << a->name();
+            }
+        }
+    }
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
